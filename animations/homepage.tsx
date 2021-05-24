0a5fecc811c1b83d203282362b035fe663fc1281
Joe Cooper <joecooper3@gmail.com>
2021-05-23 22:55:17 -0400

    Adds rope stretch easter egg, refactor

---
+++ animations/homepage.tsx
@@ -0,0 +1,40 @@
+import gsap from "gsap";
+import Matter from "matter-js";
+
+import { Shape, convertUnits } from "@utils/homepage";
+
+export const pulsate = (
+  rope: Matter.Composite,
+  userHeight: number,
+  breakFunc
+) => {
+  rope.bodies.forEach((shape: Shape) => {
+    const tl = gsap.timeline();
+    shape.activeTL = tl;
+    tl.to(shape.render, {
+      lineWidth: convertUnits(10, userHeight),
+      duration: 0.3,
+      repeat: 3,
+      yoyo: true,
+    });
+    tl.to(shape.render, {
+      lineWidth: convertUnits(15, userHeight),
+      duration: 0.2,
+      repeat: 7,
+      yoyo: true,
+    });
+    tl.add(() => {
+      breakFunc([rope]);
+    });
+  });
+};
+
+export const stopPulsating = (rope: Matter.Composite) => {
+  rope.bodies.forEach((shape: Shape) => {
+    if (shape.activeTL) {
+      shape.activeTL.pause();
+      shape.activeTL = null;
+      gsap.to(shape.render, { lineWidth: 0 });
+    }
+  });
+};
